cffbc7731283854b41c6128ee246a20db07215f8
Bayram <st141269@stud.uni-stuttgart.de>
2020-12-09 19:11:43 +0100

    initialized ads8688 libraries

---
+++ adc/ADS8688/App/ads8688.c
@@ -0,0 +1,12 @@
+/*
+ * ads8688.c
+ *
+ *  Created on: 09.12.2020
+ *      Author: Bayram
+ */
+
+#include "adc/ADS8688/App/ads8688.h"
+#ifdef STM32F4xx_HAL_SPI_H
+
+#endif /* STM32F4xx_HAL_SPI_H */
+
