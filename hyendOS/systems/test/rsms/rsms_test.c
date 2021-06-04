1f4b538cda59a93e1bf2feb4a886febc377f0024
Bayram <st141269@stud.uni-stuttgart.de>
2021-06-04 19:51:10 +0200

    some state-machine testing

--- hyendOS/systems/test/rsms/rsms_test.c
+++ hyendOS/systems/test/rsms/rsms_test.c
@@ -9,9 +9,29 @@
 
 #ifdef HYENDOS_SYSTEMS_RSMS_TEST_H_
 
+ADS_8688 adc1;
+ADS_8688 adc2;
 //Tasks idle_tasks;
 
 void rsms_test_init() {
+	initXprint(&huart2);
+	init_CAN(&hcan3, 0x111);
+
+	adc1.hspi = &hspi1;
+	adc1.cs_pin = SPI1_CS_Pin;
+	adc1.cs_port = SPI1_CS_GPIO_Port;
+	adc1.active_pins = 0b00000011;
+	adc1.input_range = ZT1V25;
+
+	adc2.hspi = &hspi2;
+	adc2.cs_pin = SPI2_CS_Pin;
+	adc2.cs_port = SPI2_CS_GPIO_Port;
+	adc2.active_pins = 0b11000011;
+	adc2.input_range = ZT1V25;
+
+	initADC(&adc1);
+	initADC(&adc2);
+	xprintf("RSMS Initialisiert.\n");
 }
 
 void rsms_responseWithData(CANBus *bus) {
--- hyendOS/systems/test/rsms/rsms_test.h
+++ hyendOS/systems/test/rsms/rsms_test.h
@@ -8,10 +8,14 @@
 
 #include <hyendOS/App/hyend_os_core.h>
 
+#if 0
+#ifdef __STM32_SPI_PERIPHERAL
 #define HYENDOS_SYSTEMS_RSMS_TEST_H_
 
 void rsms_test_init(void);
 void rsms_test_canHandle(CANBus*);
 
 //void rsms_init(Microcontroller*);
+#endif
+#endif
 
