bbdaa1e009cd56a1a59988c7ab40d9b33e87156c
Bayram <st141269@stud.uni-stuttgart.de>
2021-06-09 16:19:35 +0200

    Test GSE added

---
+++ systems/test/gse/gse_test.c
@@ -0,0 +1,15 @@
+/*
+ * gse_test.c
+ *
+ *  Created on: Jun 9, 2021
+ *      Author: Bayram
+ */
+
+#include "systems/test/gse/gse_test.h"
+
+#ifdef SYSTEMS_TEST_GSE_GSE_TEST_H_
+
+void GSE_init(){
+
+}
+
---
+++ systems/test/gse/gse_test.h
@@ -0,0 +1,12 @@
+/*
+ * gse_test.h
+ *
+ *  Created on: Jun 9, 2021
+ *      Author: Bayram
+ */
+
+#pragma once
+#define SYSTEMS_TEST_GSE_GSE_TEST_H_
+
+void GSE_init(void);
+
