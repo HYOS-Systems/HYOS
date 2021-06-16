6055ae8a1a03dab029b3704744428ff87eafe878
Bayram <st141269@stud.uni-stuttgart.de>
2021-06-16 21:37:44 +0200

    included Status Functions

--- hyendOS/canProtocolImplementation/canProtocolImpl.c
+++ hyendOS/canProtocolImplementation/canProtocolImpl.c
@@ -7,19 +7,19 @@
 
 #include "hyendOS/canProtocolImplementation/canProtocolImpl.h"
 
-#ifdef CANPROTOCOL_H_
+#ifdef CANPROTOCOLIMPL_H_
 
 CANP_Package package;
 uint8_t i;
 
 // Receiving -----------------------------------------------------------------------------
-void CANI_receiveMessage(CANBus* bus, CANP_MessageHeader* mHeader){
+void CANI_receiveMessage(CANBus *bus, CANP_MessageHeader *mHeader) {
 	CAN_ReceiveMessage(bus);
 	package.extID = bus->pRxHeader.ExtId;
 	CANP_unpackHeader(&package, mHeader);
 }
 
-uint8_t CANI_isThisTarget(CANP_MessageHeader* mHeader){
+uint8_t CANI_isThisTarget(CANP_MessageHeader *mHeader) {
 	return mHeader->targetMCU == MAX_MCU || mHeader->targetMCU == microcontroller.number;
 }
 
@@ -28,19 +28,19 @@ void CANI_fillHeader(CANP_MessageHeader *mHeader) {
 	mHeader->sourceMCU = microcontroller.number;
 }
 
-void CANI_sendMessage(CANBus* bus, CANP_MessageHeader* mHeader, uint8_t* payload){
+void CANI_sendMessage(CANBus *bus, CANP_MessageHeader *mHeader, uint8_t *payload) {
 	CAN_setHeader(bus, package.extID);
 	CAN_SendMessage(payload, bus);
 }
 
-void CANI_sendData(CANBus* bus, CANP_DataMessage* message){
+void CANI_sendData(CANBus *bus, CANP_DataMessage *message) {
 	CANI_fillHeader(&message->header);
 
 	CANP_packData(&package, message);
 	CANI_sendMessage(bus, &(message->header), package.payload);
 }
 
-void CANI_sendRequestData(CANBus* bus, CANP_RequestDataMessage* message){
+void CANI_sendRequestData(CANBus *bus, CANP_RequestDataMessage *message) {
 	CANI_fillHeader(&message->header);
 
 	message->state = microcontroller.state->stateID;
@@ -49,7 +49,7 @@ void CANI_sendRequestData(CANBus* bus, CANP_RequestDataMessage* message){
 	CANI_sendMessage(bus, &(message->header), package.payload);
 }
 
-void CANI_sendTransition(CANBus* bus, CANP_TransitionMessage* message){
+void CANI_sendTransition(CANBus *bus, CANP_TransitionMessage *message) {
 	CANI_fillHeader(&message->header);
 
 	CANP_packTransition(&package, message);
@@ -57,45 +57,49 @@ void CANI_sendTransition(CANBus* bus, CANP_TransitionMessage* message){
 }
 
 // Interpreting -----------------------------------------------------------------------------
-uint8_t CANI_isMaster(CANP_MessageHeader* mHeader){
+uint8_t CANI_isMaster(CANP_MessageHeader *mHeader) {
 	return mHeader->sourceMCU == microcontroller.master;
 }
 
-uint8_t CANI_isTransitionHeader(CANP_MessageHeader* mHeader){
+uint8_t CANI_isTransitionHeader(CANP_MessageHeader *mHeader) {
 	return mHeader->messageType == TRANSITION;
 }
 
-void CANI_copyCANpackage(CANP_Package* package, CANBus* bus){
+void CANI_copyCANpackage(CANP_Package *package, CANBus *bus) {
 	package->dlc = bus->pRxHeader.DLC;
 	package->extID = bus->pRxHeader.ExtId;
-	for(i=0; i<package->dlc; i++){
+	for (i = 0; i < package->dlc; i++) {
 		package->payload[i] = bus->receiveBuffer[i];
 	}
 }
 
-void CANI_interpretDataMessage(CANBus* bus, CANP_DataMessage* message){
+void CANI_interpretDataMessage(CANBus *bus, CANP_DataMessage *message) {
 	CANI_copyCANpackage(&package, bus);
 	CANP_unpackDataMessage(&package, message);
 }
 
-void CANI_interpretRequestDataMessage(CANBus* bus, CANP_RequestDataMessage* message){
+void CANI_interpretRequestDataMessage(CANBus *bus, CANP_RequestDataMessage *message) {
 	CANI_copyCANpackage(&package, bus);
 	CANP_unpackRequestDataMessage(&package, message);
 }
 
-void CANI_interpretTransitionMessage(CANBus* bus, CANP_TransitionMessage* message){
+void CANI_interpretTransitionMessage(CANBus *bus, CANP_TransitionMessage *message) {
 	CANI_copyCANpackage(&package, bus);
 	CANP_unpackTransitionMessage(&package, message);
-	if (CANI_isMaster(&message->header) && CANI_isTransitionHeader(&message->header) && message->messageValid){
+	if (CANI_isMaster(&message->header) && CANI_isTransitionHeader(&message->header) && message->messageValid) {
 		stateTransition(message->state);
 	}
 }
 
-void CANI_unpackStatus(CANP_Data* data, CANP_Status* status) {
+uint8_t CANI_isTransition(CANP_Data *data) {
+	return CANP_isStatusData(data);
+}
+
+void CANI_unpackStatus(CANP_Data *data, CANP_Status *status) {
 	CANP_unpackStatus(data, status);
 }
 
-void CANI_packStatus(CANP_Data* data, CANP_Status* status) {
+void CANI_packStatus(CANP_Data *data, CANP_Status *status) {
 	CANP_packStatus(data, status);
 }
 
--- hyendOS/canProtocolImplementation/canProtocolImpl.h
+++ hyendOS/canProtocolImplementation/canProtocolImpl.h
@@ -12,7 +12,6 @@
 
 #ifdef INC_CANTRI_H_
 #define CANPROTOCOLIMPL_H_
-#endif
 
 void CANI_receiveMessage(CANBus*, CANP_MessageHeader*);
 uint8_t CANI_isThisTarget(CANP_MessageHeader*);
@@ -24,3 +23,9 @@ void CANI_sendTransition(CANBus*, CANP_TransitionMessage*);
 void CANI_interpretDataMessage(CANBus*, CANP_DataMessage*);
 void CANI_interpretRequestDataMessage(CANBus*, CANP_RequestDataMessage*);
 void CANI_interpretTransitionMessage(CANBus*, CANP_TransitionMessage*);
+
+uint8_t CANI_isTransition(CANP_Data *data);
+void CANI_unpackStatus(CANP_Data *data, CANP_Status *status);
+void CANI_packStatus(CANP_Data *data, CANP_Status *status);
+
+#endif
