ca97f59d1a62559bd01c1e8ddb5ac604cec6948e
Bayram <st141269@stud.uni-stuttgart.de>
2021-06-18 23:32:31 +0200

    introduced gopro files

---
+++ peripherals/gopro/gopro.c
@@ -0,0 +1,8 @@
+/*
+ * gopro.c
+ *
+ *  Created on: Jun 18, 2021
+ *      Author: Bayram
+ */
+
+
---
+++ peripherals/gopro/gopro.h
@@ -0,0 +1,16 @@
+/*
+ * gopro.h
+ *
+ *  Created on: Jun 18, 2021
+ *      Author: Bayram
+ */
+
+#pragma once
+
+#include "hyendOS/hyend_os.h"
+#ifdef _STM32
+#define PERIPHERALS_GOPRO_GOPRO_H_
+
+
+
+#endif /* PERIPHERALS_GOPRO_GOPRO_H_ */
